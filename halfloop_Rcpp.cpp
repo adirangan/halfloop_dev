367a61ac5ba8db043d20c559697148ff97beaacc
adirangan <adirangan@yahoo.com>
2021-04-16 03:15:22 -0400

    modifying to run in Rcpp

--- halfloop_Rcpp.cpp
+++ halfloop_Rcpp.cpp
@@ -14,12 +14,6 @@ IntegerVector halfloop_test() {
   return 0;
 }
 
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
 /*** R
 halfloop_test()
 */
